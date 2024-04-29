3b76b7d8884c0a43e4a21c09b871f7b644201825
wongprom <>
2024-04-29 16:19:05 +0200

    Connect to Spotify and create new spotyfy playlist based on scrap result.

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
+import os
+from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-user_input = input("Wich year do you want to travel to? Type the data in this format YYY-MM-DD: ")
 
+load_dotenv()
+SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME")
+
+# Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt",
+        username=SPOTIFY_USERNAME,
+    )
+)
+user_id = sp.current_user()["id"]
+
+# Scraping Billboard 100
+user_input = input("Which year do you want to travel to? Type the data in this format YYY-MM-DD: ")
 URL = f"https://www.billboard.com/charts/hot-100/{user_input}"
-
 response = requests.get(URL)
 billboard_page = response.text
-
 soup = BeautifulSoup(billboard_page, 'html.parser')
 all_songs = soup.find_all(name="h3", class_="a-no-trucate")
-all_titles = [song.getText().strip() for song in all_songs]
-print(all_titles)
+song_names = [song.getText().strip() for song in all_songs]
+
+# Searching Spotify for songs by title
+song_uris = []
+year = user_input.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    # print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+
+# Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{user_input} Billboard 100", public=False)
+
+# Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
